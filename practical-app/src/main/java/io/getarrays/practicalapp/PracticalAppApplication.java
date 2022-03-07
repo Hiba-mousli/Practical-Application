37e84851754ac1fd547071fd78c3381a00740dc4
Hiba-mousli <hibamousli85@gmail.com>
2022-03-07 08:22:43 +0200

    --init spring

---
+++ practical-app/src/main/java/io/getarrays/practicalapp/PracticalAppApplication.java
@@ -0,0 +1,13 @@
+package io.getarrays.practicalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalAppApplication.class, args);
+	}
+
+}
---
+++ practical-app/src/test/java/io/getarrays/practicalapp/PracticalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.getarrays.practicalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
